19589d5e677ace3198c7b89a3f5138c16b7c93ea
Alpha-lazy <anishmore712@gmail.com>
2024-12-08 21:14:33 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ const PORT = 5000;
 const path = require('path')
 
 app.use(express.json());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // cors access
 
