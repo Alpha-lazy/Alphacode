7c9f9f853558ae62bcb01c5b7119d06a46ccf360
Alpha-lazy <anishmore712@gmail.com>
2024-12-09 19:32:35 +0530

    updated

--- Mern-adminPanel/src/store/auth.jsx
+++ Mern-adminPanel/src/store/auth.jsx
@@ -58,15 +58,15 @@ export const AuthProvider = ({children}) =>{
     // THis operator is used to convert the value in boolean
     const isloggedIn = !!token;
     if (isloggedIn) {
-        admin =token !== "undefined"?false:decodeToken(token).isAdmin  ;
+        admin =decodeToken(token).isAdmin  ;
         userid =decodeToken(token).userId;
         console.log("token is undefined");
         
     }
-    if (token !== "undefined") {
-       admin = false
-      return localStorage.removeItem('token');
-    }
+    // if (token !== "undefined") {
+    //    admin = false
+    //   return localStorage.removeItem('token');
+    // }
      
     console.log(admin);
     
