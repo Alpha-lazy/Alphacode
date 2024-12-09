69a5e9fcdd6e1aebc3bdb3a3a5eeaa40a01686b2
Alpha-lazy <anishmore712@gmail.com>
2024-12-09 09:59:09 +0530

    adding new feature

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -5,28 +5,28 @@ const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer")
 var otp;
 
-const home = async(req, res) => {
+const home = async (req, res) => {
     try {
-       
+
         if (req.modified) {
             if (req.user.isAdmin === true) {
-            let userdata = req.user
-            res.status(200).json({message:"You are admin", token: await userdata.generateToken()})
-            }
-            else{
                 let userdata = req.user
-                res.status(400).json({token: await userdata.generateToken()})
+                res.status(200).json({ message: "You are admin", token: await userdata.generateToken() })
             }
-         
-            
+            else {
+                let userdata = req.user
+                res.status(400).json({ token: await userdata.generateToken() })
+            }
+
+
         }
-        else{
+        else {
             let userdata = req.user
-            res.status(400).json({token: await userdata.generateToken()})
+            res.status(400).json({ token: await userdata.generateToken() })
         }
-       
+
     } catch (error) {
-        res.status(500).json({message:'Internal Server Error'});
+        res.status(500).json({ message: 'Internal Server Error' });
     }
 
 };
@@ -36,9 +36,9 @@ const home = async(req, res) => {
 const register = async (req, res) => {
     try {
         // we request to body for data
-        
+
         const { username, email, phone, password } = req.body;
-        const userBlocked = await Block.findOne({email:email})
+        const userBlocked = await Block.findOne({ email: email })
 
         // finding if userexist 
 
@@ -49,34 +49,34 @@ const register = async (req, res) => {
         }
         //  hash the password
         if (userBlocked) {
-            return res.status(400).json({message:"You are blocked"})
+            return res.status(400).json({ message: "You are blocked" })
         }
 
         // create and insert the data
-    
-            const usercreated = await User.create({ username, email, phone, password })
-            res.status(201).json({message:"Registartion successfull", token: await usercreated.generateToken(), id: usercreated._id.toString() }); 
-      
+
+        const usercreated = await User.create({ username, email, phone, password })
+        res.status(201).json({ message: "Registartion successfull", token: await usercreated.generateToken(), id: usercreated._id.toString() });
+
 
     } catch (error) {
-   
-        
+
+
         res.status(500).json({ message: 'internal server error' });
     }
 }
 
 // verifyemail
 
-const Emailverify = async(req,res) =>{
+const Emailverify = async (req, res) => {
 
     otp = Math.floor(1000 + Math.random() * 9000)
-    
-    
-    
+
+
+
     try {
-         
+
         const { username, email, phone, password } = req.body;
-        const userBlocked = await Block.findOne({email:email})
+        const userBlocked = await Block.findOne({ email: email })
 
         // finding if userexist 
 
@@ -87,7 +87,7 @@ const Emailverify = async(req,res) =>{
         }
         //  hash the password
         if (userBlocked) {
-            return res.status(400).json({message:"You are blocked"})
+            return res.status(400).json({ message: "You are blocked" })
         }
 
         const auth = nodemailer.createTransport({
@@ -95,52 +95,65 @@ const Emailverify = async(req,res) =>{
             port: 465,
             secure: true, // true for port 465, false for other ports
             auth: {
-              user: "anishmore712@gmail.com",
-              pass: "hfjrbxlgwefmivfm",
+                user: "alphacode400@gmail.com",
+                pass: "xxemajqizwgcoioa",
             },
-          });
-          
-          const receiver ={
-            from:"Alphacode",
-            to:`${req.body.email}`,
-            subject:"welcome to Alphacode",
-            text:`Here your otp ${otp}`
-          }
+        });
 
-          auth.sendMail(receiver,(error,emailResponcer)=>{
-             if (error) {
-                res.status(200).json({message:"please enter valide email"})
-             }
-             else{
-              res.status(200).json({message:"otp is send on your email"})
+        const receiver = {
+            from: "Alphacode",
+            to: `${req.body.email}`,
+            subject: "Verify Your Email Address",
+            text: `Hi ${username},
+                 
+                 Thank you for signing up with [Your Company Name]! To complete your registration, please verify your email using the One-Time Password (OTP) below:
+                 
+                 Your OTP: ${otp}
+                 
+                 This OTP is valid for the next [X] minutes. Please enter it on the verification page to confirm your email address.
+                 
+                 If you didn’t request this, please ignore this email or contact us immediately at alphacode400@gmail.com.
+                 We’re excited to have you onboard!
+                 
+                 Warm regards,
+                 The Alphacode Team
+                 alphacode400@gmail.com`
+        }
+
+        auth.sendMail(receiver, (error, emailResponcer) => {
+            if (error) {
+                res.status(200).json({ message: "please enter valide email" })
             }
-             
-          })
+            else {
+                res.status(200).json({ message: "otp is send on your email" })
+            }
+
+        })
     } catch (error) {
         console.log(error);
-        res.status(500).json({message:"Internal server error"})
+        res.status(500).json({ message: "Internal server error" })
     }
 
-   
 
-    
+
+
 }
 
 // verifyOtp
-const verifyOtp = (req,res) =>{
+const verifyOtp = (req, res) => {
     let userOtp = req.body.otp;
-console.log(userOtp===otp);
+    console.log(userOtp === otp);
 
     if (userOtp != otp) {
-        
-        
-       res.status(400).json({message:"Incorrect otp"})
+
+
+        res.status(400).json({ message: "Incorrect otp" })
     }
-    else{
-        res.status(200).json({message:"Registration successfull"})
+    else {
+        res.status(200).json({ message: "Registration successfull" })
     }
     // res.s?
-   
+
 }
 // post request for login form
 
@@ -148,21 +161,21 @@ const login = async (req, res) => {
     try {
         const user = req.body;
         const userExist = await User.findOne({ email: user.email });
-        
+
 
         // finde user with this email is exist in database
         if (!userExist) {
             return res.status(400).json({ message: "invalid cradentials" });
         }
 
-        
+
         // if user exist in the database then compare the passsword
 
         const userPassword = await userExist.comparePassword(user.password);
 
         if (userPassword) {
             try {
-            //   if user passwrod matched with database then generate token
+                //   if user passwrod matched with database then generate token
 
                 res.status(200).json({
                     msg: "Login successful",
@@ -186,12 +199,12 @@ const login = async (req, res) => {
 }
 
 
-const userdata = async(req,res) =>{
-       const userdata = req.user;
-       
-       res.status(200).json(userdata)
+const userdata = async (req, res) => {
+    const userdata = req.user;
+
+    res.status(200).json(userdata)
 }
 
 
 
-module.exports = { home, register, login ,userdata, Emailverify, verifyOtp};
+module.exports = { home, register, login, userdata, Emailverify, verifyOtp };
