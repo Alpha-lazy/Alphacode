08320fcffe14aee46ea53d841e72367c5c1fa726
Alpha-lazy <anishmore712@gmail.com>
2024-12-10 21:03:36 +0530

    updated

--- Mern-adminPanel/src/component/Navbar.jsx
+++ Mern-adminPanel/src/component/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
                         : (
                             <>
                             {/* <li><NavLink to="/register">Signup</NavLink></li> */}
-                            <li><NavLink to="/login">Login</NavLink></li>
+                            {/* <li><NavLink to="/login">Login</NavLink></li> */}
                             </>
                             )}
                         
