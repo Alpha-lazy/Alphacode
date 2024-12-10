d2e61df84339f1fec93bd054728061ab0af166dc
Alpha-lazy <anishmore712@gmail.com>
2024-12-10 20:01:38 +0530

    updated

--- Mern-adminPanel/src/pages/Admin-User.jsx
+++ Mern-adminPanel/src/pages/Admin-User.jsx
@@ -122,10 +122,9 @@ const editdata =  (editId) =>{
   
 }
  
-const addData = () =>{
-   console.log("clicked");
-   
-    // return <Navigate to="/register"/>
+function adddata() {
+  console.log('clicked');
+  
 }
    
 
@@ -170,7 +169,7 @@ const addData = () =>{
             })
           }
 
-              <button style={{ width: "100px", height: "30px",border:"none", borderRadius: "5px", backgroundColor: "#0571c9", fontSize: "15px", cursor: "pointer", fontWeight: "500", color: "white" }} onClick={addData()}>Add User +</button>
+              <button style={{ width: "100px", height: "30px",border:"none", borderRadius: "5px", backgroundColor: "#0571c9", fontSize: "15px", cursor: "pointer", fontWeight: "500", color: "white" }} onClick={adddata()}>Add User +</button>
          </tbody>
       
       </table>
