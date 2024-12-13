735fd0c61e5ebfbed48cc1d74501e415b0bb8b0e
Alpha-lazy <anishmore712@gmail.com>
2024-12-13 11:58:26 +0530

    updated

--- Mern-adminPanel/src/pages/Add-post.jsx
+++ Mern-adminPanel/src/pages/Add-post.jsx
@@ -9,6 +9,8 @@ function Addpost() {
         title:"",
         content:""
     })
+    console.log(postData.title);
+    
 
     const handleInput = (e) => {
           let name = e.target.name;
@@ -40,9 +42,9 @@ function Addpost() {
                 <div className={css.formconatainer}>
                     <form action="" className={css.form} onSubmit={handleForm} method="post">
 
-                        <input type="text" name="title" value={postData.title} onChange={handleInput} required id="title" />
+                        <input type="text" name="title"  onChange={handleInput} required id="title" />
                         <label htmlFor="name">Title</label>
-                        <textarea id="message" name="content" value={postData.content} onChange={handleInput} required rows="4" cols="60" placeholder='Type your message'></textarea>
+                        <textarea id="message" name="content"  onChange={handleInput} required rows="4" cols="60" placeholder='Type your message'></textarea>
                         <label htmlFor="message" className='messageLabel' style={{ marginTop: "-142px" }}>Your Content</label>
 
 
