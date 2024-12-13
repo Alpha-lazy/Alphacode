98edded36c89fc6747c5b44449309cf2c42d25eb
Alpha-lazy <anishmore712@gmail.com>
2024-12-13 20:57:21 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -11,9 +11,11 @@ const app = express();
 const PORT = 5000;
 const path = require('path')
 const PostRoute = require('./router/post-route')
+const bodyparse = require('body-parser')
+
 app.use(express.json());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyparse.json({ limit: '50mb' }));
+app.use(bodyparse.urlencoded({ limit: '50mb', extended: true }));
 
 // cors access
 
